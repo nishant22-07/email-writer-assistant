c50d9b8cc829df4c95c548054666f3acda089735
nishant <nishantkudale04@gmail.com>
2025-09-02 16:56:42 +0530

    Create REST controller to handle email generation requests

---
+++ src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -0,0 +1,40 @@
+package com.email.writer.app;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email/")
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping
+    public ResponseEntity<String> generateEmail(@RequestBody Emailrequest emailrequest ){
+        String response = emailGeneratorService.generateEmailReply(emailrequest);
+        return ResponseEntity.ok(response);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
